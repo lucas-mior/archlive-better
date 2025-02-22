5ee55cdf78b96c61d812387ea3f44a9794b90406
lucas-mior <lucas.mior@tutamail.com>
2025-02-22 10:42:31 -0300

    wip

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# set -e
 if [ -z "$1" ]; then
     echo "usage: $0 <output directory>"
     exit 1
@@ -16,16 +17,26 @@ fi
 
 sudo bash -c "rm -rf /$work/*"
 
-wiki_install="$dir/airootfs/root/install.html"
-wiki_network="$dir/airootfs/root/network.html"
+install="$dir/airootfs/root/install.html"
+network="$dir/airootfs/root/network.html"
 
-if [ "$(find "$wiki_install" -mtime +2)" ] || [ ! -e "$wiki_install" ]; then
-    wget -qO - "https://wiki.archlinux.org/title/Installation_guide" \
-        > "$wiki_install"
-fi
-if [ "$(find "$wiki_network" -mtime +2)" ] || [ ! -e "$wiki_network" ]; then
-    wget -qO - "https://wiki.archlinux.org/title/Network_configuration" \
-        > "$wiki_network"
-fi
+wiki_domain="wiki.archlinux.org"
+
+wget -qO - "https://${wiki_domain}/title/Installation_guide" > "$install"
+wget -qO - "https://${wiki_domain}/title/Network_configuration" > "$network"
+
+{
+    rm -rf surf custom
+    git clone https://aur.archlinux.org/surf.git
+    cd surf
+    PKGEXT=".pkg.tar" makepkg
+    cd ..
+    mkdir ./custom
+    mv surf/surf-2.1-6-x86_64.pkg.tar custom/
+    repo-add custom/custom.db.tar.zst custom/surf-2.1-6-x86_64.pkg.tar || exit
+    if ! grep -q "^surf$" ./packages.x86_64 ; then
+        echo "surf" >> ./packages.x86_64 
+    fi
+}
 
 sudo mkarchiso -v -w "$work" -o "$iso_dir" "$dir"
